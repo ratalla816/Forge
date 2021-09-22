d93746288544828ce1c837d56043a63c5f74ae38
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-22 18:17:51 -0500

    div target correct

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,11 +1,11 @@
-const parallax = document.getElementById ("parallax");
+// const parallax = document.getElementById ("parallax");
 
-window.addEventListener ("scroll", function ()
-{ let offset = window.pageYOffset;
-//   console.log ('Offset: ' + offset);
-    console.log('offset * 0.7' + offset * 0.7);
-    parallax.style.backgroundPositionY = offset * 0.7 + "px"; 
-})
+// window.addEventListener ("scroll", function ()
+// { let offset = window.pageYOffset;
+// //   console.log ('Offset: ' + offset);
+//     console.log('offset * 0.7' + offset * 0.7);
+//     parallax.style.backgroundPositionY = offset * 0.7 + "px"; 
+// })
 
 
 function myFunction() {
@@ -20,5 +20,13 @@ function myFunction() {
     middle.classList.toggle("dark-mode");
     right.classList.toggle("dark-mode");
  }
+
+ $('#middle-panel').stellar({
+    scrollProperty: 'transform'
+  });
+
+  // <script src="https://cdnjs.cloudflare.com/ajax/libs/stellar.js/0.6.2/jquery.stellar.min.js" 
+//         integrity="sha512-PNXCBnFH9wShbV+mYnrfo0Gf3iSREgBWmYAoMIfc+Z83vVq3Nu4yxBk6j+BZ40ZIhtW3WlTQdBvW3AYLAnlgpA==" 
+//         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
  
