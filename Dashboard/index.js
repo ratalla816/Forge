7091ae565471771aff95ee87b633ed7a691dd2e2
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-22 13:58:51 -0500

    added js

--- Dashboard/index.js
+++ Dashboard/index.js
@@ -1,3 +1,12 @@
+const parallax = document.getElementById ("parallax");
+
+window.addEventListener ("scroll", function ()
+{ let offset = window.pageYOffset;
+//   console.log ('Offset: ' + offset);
+    console.log('offset * 0.7' + offset * 0.7);
+    parallax.style.backgroundPositionY = offset * 0.7 + "px"; 
+})
+
 function myFunction() {
     var element = document.body;
     var container = document.getElementsByClassName('container');
