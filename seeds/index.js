b36885690f6d8b64a24e9f99f5bde92f4ef92de4
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-23 14:42:55 -0500

    updated server.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,4 @@
-const seed_name = require('./'); // Add directory of seeds and add a const name
-
-const sequelize = require('../config/connection');
-
-const seedAll = async () => {
-    await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
-    await x(); // add function to seed change x to some name
-    console.log('\n----- comment -----\n');
+const seed_name = require('./');
   
-    process.exit(0);
-  };
-  
-  seedAll();
+
+seedAll();
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'The duck is waddling',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -20,20 +20,17 @@ const sess = {
 };
 
 app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const helpers = require('./utils/helpers');
-
 const hbs = exphbs.create({ helpers });
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Should be listening on port 3001, hopefully'));
 });
