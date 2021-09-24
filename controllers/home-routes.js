10f2d2471114b004611cb2eade613c174347ea4a
audryf <84256033+audryf@users.noreply.github.com>
2021-09-23 21:50:12 -0500

    update pageroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,28 +2,35 @@ const router = require('express').Router();
 // const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// get all posts for splashpage and render
 router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: ['id', 'title', 'body', 'created_at'],
-        include: {
-            model: User,
-            attributes: ['username']
-        }
-    }).then(postData => {
-        const posts = postData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
-    }).catch(err => res.status(500).json(err));
+
+ res.render('splashpage')
 });
-
-// // get all posts for splashpage and render
-// router.get('/', (req, res) => {
-
-//  res.render('splashpage')
-// });
 
 // get homepage and render
 router.get('/homepage', (req, res) => {
-res.render('homepage', posts)
+    Post.findAll({
+        order: [['created_at', 'DESC']],
+        attributes: ['id', 'title', 'url', 'body', 'created_at'],
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'body'],
+            indlude: {
+              model: User,
+              attributes: ['username']
+            }
+          }
+        ]
+      }).then(postData => {
+        const posts = postData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    }).catch(error => res.status(500).json(error));
 });
 
 // get single post for single post page
